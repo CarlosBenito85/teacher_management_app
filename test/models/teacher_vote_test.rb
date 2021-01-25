13fa702548b197232a82d2fc2e8131792188015d
Carlos Benito <carlos.benito85@gmail.com>
2021-01-25 01:35:31 +0000

    Refactoring

--- test/models/teacher_vote_test.rb
+++ test/models/teacher_vote_test.rb
@@ -3,21 +3,21 @@ require 'test_helper'
 class TeacherVoteTest < ActiveSupport::TestCase
 
   def setup
-    @teacher_vote = TeacherVote.new(voter_id: teachers(:test_teacher).id,
-                                    voted_id: teachers(:test_professor).id)
+    @teacher_vote = TeacherVote.new(voter_teacher_id: teachers(:test_teacher).id,
+                                    voted_teacher_id: teachers(:test_professor).id)
   end
 
   test "should be valid" do
     assert @teacher_vote.valid?
   end
 
-  test "should require a voter_id" do
-    @teacher_vote.voter_id = nil
+  test "should require a voter_teacher_id" do
+    @teacher_vote.voter_teacher_id = nil
     assert_not @teacher_vote.valid?
   end
 
-  test "should require a voted_id" do
-    @teacher_vote.voted_id = nil
+  test "should require a voted_teacher_id" do
+    @teacher_vote.voted_teacher_id = nil
     assert_not @teacher_vote.valid?
   end
   
